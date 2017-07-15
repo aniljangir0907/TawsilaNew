c1549547e869078c84aa94e238bc524ad052d633
vikram singh charan <vc7662@gmail.com>
2017-07-15 09:36:40 +0530

    commtait
    homeview controller

--- Tawsila/Appdelegate/AppDelegate.swift
+++ Tawsila/Appdelegate/AppDelegate.swift
@@ -140,7 +140,7 @@
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        self.application(application, didFinishLaunchingWithOptions: nil)
+       /// self.application(application, didFinishLaunchingWithOptions: nil)
     }
     
     func applicationWillEnterForeground(_ application: UIApplication) {
